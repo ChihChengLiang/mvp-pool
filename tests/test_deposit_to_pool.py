a2d091b81b6ef81151e8acb4d0f7b962b49bd433
Chih Cheng Liang <chihchengliang@gmail.com>
2018-05-17 18:24:51 +0800

    [minor] remove unused variable

--- tests/test_deposit_to_pool.py
+++ tests/test_deposit_to_pool.py
@@ -3,7 +3,7 @@ from ethereum import utils
 def test_deposit_to_pool(casper_chain, casper, new_epoch, pool, funded_privkey, deposit_amount,
                          depositor_privkey, depositor_deposit_amount, induct_validator):
 
-    validator_index = induct_validator(funded_privkey, deposit_amount)
+    induct_validator(funded_privkey, deposit_amount)
 
     assert pool.CASPER_ADDR() == '0x' + utils.encode_hex(casper.address)
     assert pool.next_depositor_index() == 1
