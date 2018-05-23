8f494f13259fd0acdf0f342abb640eb475612fef
Chih Cheng Liang <chihchengliang@gmail.com>
2018-05-23 18:13:53 +0800

    test min_total_deposit

--- tests/test_deposit_to_casper.py
+++ tests/test_deposit_to_casper.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_deposit_to_casper(casper_chain, casper, pool, induct_validators_and_depositors,
                            depositor_privkeys, depositor_deposit_amount, operator, validation_addr, mine_until):
 
@@ -15,3 +18,29 @@ def test_deposit_to_casper(casper_chain, casper, pool, induct_validators_and_dep
     pool.deposit_to_casper(operator_valcode_addr, sender=operator["key"])
 
     assert casper.validator_indexes(pool.address) == pool.validator_index()
+
+
+@pytest.mark.parametrize(
+    'min_deposit_size,min_total_deposit,depositor_deposit_amount,success',
+    [
+        (1000*10**18, 1000*10**18, 1000*10**18, True),
+        (1000*10**18, 1000*10**18, 500*10**18, False),
+        (1000*10**18, 1500*10**18, 1000*10**18, False)
+    ]
+)
+def test_min_total_deposit(min_deposit_size, min_total_deposit, depositor_deposit_amount, success,
+                           casper_chain, casper, pool, induct_validators_and_depositors,
+                           depositor_privkeys, operator, validation_addr, mine_until, assert_tx_failed):
+
+    n_depositor = len(depositor_privkeys)
+    induct_validators_and_depositors(depositor_privkeys,
+                                     [int(depositor_deposit_amount/n_depositor)]*n_depositor)
+
+    operator_valcode_addr = validation_addr(operator["key"])
+    mine_until(pool.DEPOSIT_END() + 1)
+
+    if success:
+        pool.deposit_to_casper(operator_valcode_addr, sender=operator["key"])
+    else:
+        assert_tx_failed(lambda: pool.deposit_to_casper(
+            operator_valcode_addr, sender=operator["key"]))
